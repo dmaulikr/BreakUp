7e953f6fc8e0a7457d2993b9eac59dae05cc1475
Randall Lee <randallee@gmail.com>
2015-09-01 15:19:44 -0500

    GameCenter Initial Commit
    GameCenter Initial Commit
    
    Added GameKit.framework and started on singleton class for User Auth

---
+++ BreakUp/BreakUp/GCUserAuthHelper.h
@@ -0,0 +1,13 @@
+//
+//  GCUserAuthHelper.h
+//  BreakUp
+//
+//  Created by Apple on 9/1/15.
+//  Copyright (c) 2015 Randall Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCUserAuthHelper : NSObject
+
+@end
---
+++ BreakUp/BreakUp/GCUserAuthHelper.m
@@ -0,0 +1,13 @@
+//
+//  GCUserAuthHelper.m
+//  BreakUp
+//
+//  Created by Apple on 9/1/15.
+//  Copyright (c) 2015 Randall Lee. All rights reserved.
+//
+
+#import "GCUserAuthHelper.h"
+
+@implementation GCUserAuthHelper
+
+@end
